440883bd227f32d3ad81ef7386eea33b9135b484
sydkaka <sydkaka@icloud.com>
2015-04-22 11:10:55 -0700

    Initial Commit

---
+++ p7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p7
+//
+//  Created by SunYongding on 4/22/15.
+//  Copyright (c) 2015 SunYongding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
